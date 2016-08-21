a8970357c0a7fc34b11765aace85fbcd758a3b59
谭晓灿 <xiaocantan@sohu-inc.com>
2016-08-21 16:05:19 +0800

    支持转换exclusion,支持获取version

--- src/test/java/ConfigBeanTest.java
+++ src/test/java/ConfigBeanTest.java
@@ -1,9 +1,13 @@
 import com.txc.conf.ConfigBean;
+import com.txc.conf.ConfigBeanSupport;
+import com.txc.util.GradleGenerator;
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 /**
@@ -13,8 +17,11 @@ public class ConfigBeanTest {
     @Test
     public void testConfigBean(){
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("springContext.xml");
-        Properties configBean = (Properties) applicationContext.getBean("configBean");
-        System.out.println(configBean.get("dependencyManagement.dependencies"));
-        Assert.assertNotNull(configBean);
+//        Map<String,String> configBean = (Map<String,String>) applicationContext.getBean("configBeanSupport");
+//        System.out.println(configBean);
+//        Assert.assertNotNull(configBean);
+        GradleGenerator gradleGenerator = (GradleGenerator)applicationContext.getBean("gradleGenerator");
+        String result = gradleGenerator.generatorGradleBuildFile("D:\\poi\\pom-file\\pom.xml");
+        System.out.print(result);
     }
 }
